7e00f6ea9aaa0a0eaa1cfa23bb84d17d6f2c516d
Coomy Chang <coomysky@gmail.com>
2024-12-12 01:12:52 +0800

    chore: remove not use files

--- waterballs/libs/pokeCompair/src/lib/poke-compair.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pokeCompair } from './poke-compair';
-
-describe('pokeCompair', () => {
-  it('should work', () => {
-    expect(pokeCompair()).toEqual('pokeCompair');
-  });
-});
--- waterballs/libs/pokeCompair/src/lib/poke-compair.ts
+++
@@ -1,3 +0,0 @@
-export function pokeCompair(): string {
-  return 'pokeCompair';
-}
